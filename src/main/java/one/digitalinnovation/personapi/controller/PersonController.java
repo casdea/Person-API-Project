166687e8c4de8d7352d292c3af1988db08633f54
Carlos Andrade <casdea@gmail.com>
2021-07-24 11:33:11 -0300

    Criando arquivo de configurações

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personalapi.controller;
 
+import one.digitalinnovation.personalapi.entity.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
